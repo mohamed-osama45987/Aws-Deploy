0cab6c44c071c3642d5dedd9759b05c9e078c4a0
Mohamed alshami <mo.mohamed45987@gmail.com>
2023-01-09 01:19:13 +0200

    settingup the project

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -1,12 +1,14 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
+//@ts-ignore
 export const sequelize = new Sequelize({
   username: config.username,
   password: config.password,
   database: config.database,
   host: config.host,
-
+  port: config.databaseport,
   dialect: "postgres",
   storage: ":memory:",
 });
+      
